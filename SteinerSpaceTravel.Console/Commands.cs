df28a1d6b3c5fc790992dbcaa0b312f1b62a4266
terry_u16 <terry-u16@users.noreply.github.com>
2022-06-20 23:31:20 +0900

    ジャッジコンソールを追加

--- SteinerSpaceTravel.Console/Commands.cs
+++ SteinerSpaceTravel.Console/Commands.cs
@@ -1,4 +1,6 @@
 ﻿using SteinerSpaceTravel.Core.Generators;
+using SteinerSpaceTravel.Core.Judges;
+using SteinerSpaceTravel.Core.Parsers;
 using System.Text;
 
 namespace SteinerSpaceTravel.Console;
@@ -6,7 +8,7 @@ namespace SteinerSpaceTravel.Console;
 public class Commands : ConsoleAppBase
 {
     [Command("gen", "Generate testcases.")]
-    public void Generate([Option(0, "path to seed file.")] string path)
+    public void GenerateTestCases([Option(0, "path to seed file.")] string path)
     {
         const string directoryPath = "in";
 
@@ -17,13 +19,14 @@ public class Commands : ConsoleAppBase
                 Directory.CreateDirectory(directoryPath);
             }
 
-            if (!File.Exists(path))
+            if (!CheckFileExistence(path))
             {
-                System.Console.WriteLine($"No such file: {path}");
                 return;
             }
 
             using var reader = new StreamReader(path, Encoding.UTF8);
+
+            var outputCount = 0;
 
             while (reader.ReadLine() is string line)
             {
@@ -35,18 +38,64 @@ public class Commands : ConsoleAppBase
 
                 if (!ulong.TryParse(line, out var seed))
                 {
-                    System.Console.WriteLine($"Parse failed: {line}");
+                    System.Console.WriteLine($"{line}行目のパースに失敗しました。");
                     return;
                 }
 
                 var testCase = TestCaseGenerator.Generate(seed);
                 var outputPath = Path.Join(directoryPath, $"{seed:0000}.txt");
                 File.WriteAllText(outputPath, testCase + Environment.NewLine);
+                outputCount++;
             }
+
+            System.Console.WriteLine($"{outputCount}件のテストケースを生成しました。");
         }
         catch (IOException ex)
         {
             System.Console.WriteLine(ex.Message);
+        }
+    }
+
+    [Command("judge", "Judge a testcase.")]
+    public void JudgeTestCase([Option(0, "Path to input file.")] string inputPath,
+        [Option(1, "Path to output file.")] string outputPath)
+    {
+        try
+        {
+            if (!CheckFileExistence(inputPath) || !CheckFileExistence(outputPath))
+            {
+                return;
+            }
+
+            var testCaseText = File.ReadAllLines(inputPath);
+            var solutionText = File.ReadAllLines(outputPath);
+
+            var testCase = TestCaseParser.Parse(testCaseText);
+            var solution = SolutionParser.Parse(testCase, solutionText);
+            var score = Judge.CalculateScore(solution);
+
+            System.Console.WriteLine($"Score: {score}");
+        }
+        catch (IOException ex)
+        {
+            System.Console.WriteLine(ex);
+        }
+        catch (ParseFailedException ex)
+        {
+            System.Console.WriteLine(ex);
+        }
+    }
+
+    private static bool CheckFileExistence(string path)
+    {
+        if (File.Exists(path))
+        {
+            return true;
+        }
+        else
+        {
+            System.Console.WriteLine($"ファイルが存在しません: {path}");
+            return false;
         }
     }
 }
