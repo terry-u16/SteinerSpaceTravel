375b897ec6d829f7676ad6e8ee10466179844095
terry_u16 <terry-u16@users.noreply.github.com>
2022-06-23 00:38:51 +0900

    string.Split()の呼び方を修正

--- SteinerSpaceTravel.Core/Parsers/ParseExtension.cs
+++ SteinerSpaceTravel.Core/Parsers/ParseExtension.cs
@@ -3,5 +3,5 @@
 internal static class ParseExtension
 {
     public static string[] SplitAndTrim(this string s)
-        => s.Split(' ').Select(t => t.Trim()).Where(t => t.Length > 0).ToArray();
+        => s.Split(Array.Empty<char>(), StringSplitOptions.RemoveEmptyEntries);
 }
