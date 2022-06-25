23a9fae3055649fc34dc208d5f809243c1cdbdd2
terry_u16 <terry-u16@users.noreply.github.com>
2022-06-25 12:33:28 +0900

    リファクタリング

--- SteinerSpaceTravel.Web/Services/VisualizeService.cs
+++ SteinerSpaceTravel.Web/Services/VisualizeService.cs
@@ -1,4 +1,6 @@
-﻿using SkiaSharp;
+﻿using Cysharp.Web;
+using Microsoft.JSInterop;
+using SkiaSharp;
 using SteinerSpaceTravel.Core;
 using SteinerSpaceTravel.Core.Generators;
 using SteinerSpaceTravel.Core.Judges;
@@ -52,15 +54,18 @@ public class VisualizeService
 
     private Solution? _solution;
 
+    private readonly IJSRuntime _jsRuntime;
+
     private static readonly string[] NewLines = { "\r\n", "\r", "\n" };
 
-    public VisualizeService()
+    public VisualizeService(IJSRuntime jsRuntime)
     {
         _input = string.Empty;
         _output = string.Empty;
         ErrorMessage = string.Empty;
         Seed = 0;
         Score = 0;
+        _jsRuntime = jsRuntime;
         CalculateScore();
     }
 
@@ -119,5 +124,16 @@ public class VisualizeService
         {
             ErrorMessage = $"({message})";
         }
+    }
+
+    public async Task Tweet()
+    {
+        // https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent
+        const string webIntentUrl = "https://twitter.com/intent/tweet";
+        const string url = "https://google.com/";
+        const string hashtags = "test,visualizer";
+        var text = $"XXXXXのseed={Seed}で{Score:#,##0}点を獲得しました！（ここに画像を貼ってね）";
+        var tweetUrl = WebSerializer.ToQueryString(webIntentUrl, new { text, url, hashtags });
+        await _jsRuntime.InvokeVoidAsync("open", tweetUrl, "_blank", "noopener");
     }
 }
