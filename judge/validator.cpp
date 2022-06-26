3c82f5bab3c4e1127eba81a7bf18f677c155989d
terry_u16 <terry-u16@users.noreply.github.com>
2022-06-26 18:01:59 +0900

    テストケースバリデーションを追加

--- judge/validator.cpp
+++ judge/validator.cpp
@@ -1,19 +1,22 @@
+#define FOR_LINUX
+#include <cassert>
 #include <set>
 #include <tuple>
-#include <cassert>
+
 #include "testlib.h"
 
 int main(int argc, char** argv) {
     registerValidation(argc, argv);
 
     int n = inf.readInt(100, 100);
+    inf.readSpace();
     int m = inf.readInt(8, 8);
     inf.readEoln();
     std::set<std::pair<int, int>> set;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         int a = inf.readInt(0, 1000);
+        inf.readSpace();
         int b = inf.readInt(0, 1000);
         inf.readEoln();
 
@@ -23,5 +26,5 @@ int main(int argc, char** argv) {
     assert(set.size() == n);
 
     inf.readEof();
-    return 0;    
+    return 0;
 }
