6325121045864a0a975c11f39636939287ebd299
terry_u16 <terry-u16@users.noreply.github.com>
2022-06-30 22:35:20 +0900

    テスト用解法を追加

---
+++ solver_py/no_back.py
@@ -0,0 +1,8 @@
+for _ in range(8):
+    print("0 0")
+
+N = 100
+print(N)
+
+for i in range(N):
+    print(f"1 {i % 100 + 1}")
---
+++ solver_py/vmax_one.py
@@ -0,0 +1,10 @@
+for _ in range(8):
+    print("0 0")
+
+V = 100000
+print(V)
+
+for i in range(V):
+    print(f"1 {i % 100 + 1}")
+
+print(f"1 1")
