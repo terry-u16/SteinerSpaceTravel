35de358afe93338631da40d50cc8dbed28346884
terry_u16 <terry-u16@users.noreply.github.com>
2022-07-02 12:31:07 +0900

    ジャッジ検証用コード追加

---
+++ solver_py/4sq.py
@@ -0,0 +1,12 @@
+print("0 0")
+print("500 0")
+print("1000 0")
+print("1000 500")
+print("1000 1000")
+print("500 1000")
+print("0 1000")
+print("0 500")
+print(101)
+
+for i in range(101):
+    print(f"1 {i % 100 + 1}")
